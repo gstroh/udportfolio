5f1e407d879a3fee82dce8114ca3cb246485f1fc
Gregg Strohkorb <greggstrohkorb@Greggs-MacBook-Pro-9.local>
2016-01-20 18:06:31 -0600

    Changes to optimize website performance.  Phase 1.

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 720,
+            suffix: '_2x',
+            quality: 50
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['*.{gif,jpg,png}'],
+          cwd: 'images_src/',
+          dest: 'images/'
+        }]
+      }
+    },
+
+    imagemin: {                          // Task
+      dynamic: {                         // Another target
+        files: [{
+         expand: true,                  // Enable dynamic expansion
+         cwd: 'images/',                   // Src matches are relative to this path
+         src: ['**/*.{png,jpg,gif}'],   // Actual patterns to match
+         dest: 'images_min/'                  // Destination path prefix
+        }]
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.registerTask('default', ['responsive_images','imagemin']);
+
+};
